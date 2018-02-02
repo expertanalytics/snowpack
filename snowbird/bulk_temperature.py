5a23271d7819dd85c0a1809175efa6cc51c608a0
Ola Skavhaug <ola@xal.no>
2018-02-02 15:36:36 +0100

    air mass simulator rewritten

--- snowbird/bulk_temperature.py
+++ snowbird/bulk_temperature.py
@@ -8,6 +8,7 @@ from fenics import DirichletBC
 from fenics import Mesh
 from fenics import UnitIntervalMesh
 from fenics import dot, grad, dx, rhs, lhs, near, solve
+from ufl.form import Form
 import matplotlib.pylab as plt
 
 
@@ -28,29 +29,25 @@ class BulkTemperatureParameters:
         self.dt = Constant(dt)
 
 
-
 class BulkTemperature:
 
     def __init__(self, *, params: BulkTemperatureParameters) -> None:
         self.params = params
 
-    def construct_variation_problem(self) -> Tuple[Any, Any]:
+    def construct_variation_problem(self) -> Tuple[Form, Form]:
         p = self.params
         u_t = (p.T - p.T_prev)/p.dt
         f = p.Q_pc + p.Q_sw + p.Q_mm
         F = p.P_s*p.c_s*u_t*p.v*dx + p.k_e*dot(grad(p.T), grad(p.v))*dx - f*p.v*dx
         return lhs(F), rhs(F)
 
-
     @staticmethod
     def surface_boundary(x, on_boundary: bool) -> bool:
             return on_boundary and x[0] > 0
 
-
     @staticmethod
     def ground_boundary(x, on_boundary: bool) -> bool:
             return on_boundary and near(x[0], 0.0)
-
 
     @classmethod
     def make_bcs(cls, V, surface_val, ground_val) -> List[DirichletBC]:
@@ -59,7 +56,8 @@ class BulkTemperature:
         return [bc1, bc2]
 
 
-def step(*, time: float, dt: float, t_prev: Function, t: Function, a, l, bc) -> Tuple[float, Function, Function]:
+def step(*, time: float, dt: float, t_prev: Function, t: Function,
+         a: Form, l: Form, bc) -> Tuple[float, Function, Function]:
         time += dt
         solve(a == l, t, bc)
         t_prev.assign(t)
--- snowbird/mass_air_phase.py
+++ snowbird/mass_air_phase.py
@@ -1,69 +1,97 @@
-from __future__ import print_function
-from fenics import *
-from snow_mesh import simple_mesh
+from typing import Optional, Tuple
+from fenics import FunctionSpace
+from fenics import TrialFunction
+from fenics import TestFunction
+from fenics import Function
+from fenics import Constant
+from fenics import AutoSubDomain
+from fenics import FacetFunction
+from fenics import Measure
+from fenics import Mesh
+from fenics import UnitIntervalMesh
+from fenics import dot, grad, dx, rhs, lhs, near, solve, project
+from ufl.form import Form
 import matplotlib.pylab as plt
 
 
-def simple_setup(mesh, dt_val, V=None):
-    V = V or FunctionSpace(mesh, "Lagrange", 1)
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    P_prev = Function(V)
-    theta_a = Constant(1.0)
-    D_e = Constant(1.0)
-    P_d = Constant(1.0)
-    my_v = Constant(1.0)
-    my_d = Constant(1.0)
-    alpha_th = Constant(1.0)
-    M_mm = Constant(1.0)
-    q_h = Constant(1.0)
-    T_s = project(Constant(-5.0), V)
-    dt = Constant(dt_val)
-    return V, u, v, dt, P_prev, theta_a, D_e, P_d, my_v, my_d, alpha_th, M_mm, q_h, T_s
+class MassAirPhaseParameters:
+
+    def __init__(self, *,  V: Optional[FunctionSpace], mesh: Mesh, dt: float):
+        V = V or FunctionSpace(mesh, "Lagrange", 1)
+        self.V = V
+        self.u = TrialFunction(V)
+        self.v = TestFunction(V)
+        self.P_prev = Function(V)
+        self.theta_a = Constant(1.0)
+        self.D_e = Constant(1.0)
+        self.P_d = Constant(1.0)
+        self.my_v = Constant(1.0)
+        self.my_d = Constant(1.0)
+        self.alpha_th = Constant(1.0)
+        self.M_mm = Constant(1.0)
+        self.q_h = Constant(1.0)
+        self.T_s = project(Constant(-5.0), V)
+        self.dt = Constant(dt)
+        self.mesh = mesh
 
 
-def make_ds(mesh):
-    Surface = AutoSubDomain(lambda x: "on_boundary" and near(x[0], 1))
-    Allboundaries = DomainBoundary()
-    boundaries = FacetFunction("size_t", mesh)
-    boundaries.set_all(0)
-    Surface.mark(boundaries, 1)
-    ds = Measure("ds", subdomain_data = boundaries)
-    return ds
 
-def construct_variation_problem(mesh, u, v, dt, P_prev, theta_a, D_e, P_d, my_v, my_d, alpha_th, M_mm, q_h, T_s):
-    ds = make_ds(mesh)
-    u_t = (u - P_prev)/dt
-    term = alpha_th*u*P_d*(my_v + my_d)**2/((P_prev + P_d)*my_v*my_d)/T_s*grad(T_s)
-    F = theta_a*u_t*v*dx + theta_a*D_e*dot(grad(u) - term, grad(v))*dx - M_mm*v*dx + q_h*v*ds(1)
-    return lhs(F), rhs(F)
+class MassAirPhase:
+
+    def __init__(self, *, params: MassAirPhaseParameters):
+        self.params = params
+
+    @staticmethod
+    def make_ds(mesh):
+        surface = AutoSubDomain(lambda x: "on_boundary" and near(x[0], 1))
+        boundaries = FacetFunction("size_t", mesh)
+        boundaries.set_all(0)
+        surface.mark(boundaries, 1)
+        ds = Measure("ds", subdomain_data=boundaries)
+        return ds
+
+    def construct_variation_problem(self) -> Tuple[Form, Form]:
+        p = self.params
+        ds = self.make_ds(p.mesh)
+        u_t = (p.u - p.P_prev)/p.dt
+        term = p.alpha_th*p.u*p.P_d*(p.my_v + p.my_d)**2/((p.P_prev + p.P_d)*p.my_v*p.my_d)/p.T_s*grad(p.T_s)
+        F = p.theta_a*u_t*p.v*dx + p.theta_a*p.D_e*dot(grad(p.u) - term, grad(p.v))*dx - p.M_mm*p.v*dx + p.q_h*p.v*ds(1)
+        return lhs(F), rhs(F)
+
+    @staticmethod
+    def surface_boundary(x, on_boundary):
+            return on_boundary and x[0] > 0
 
 
-def surface_boundary(x, on_boundary):
-        return on_boundary and x[0] > 0
+def step(*, time: float, dt: float, p_prev: Function, p: Function,
+         a: Form, l: Form) -> Tuple[float, Function, Function]:
+        time += dt
+        solve(a == l, p)
+        p_prev.assign(p)
+        return time, p_prev, p
 
 
-def step(t, dt, P_prev, P, a, L):
-        t += dt
-        solve(a == L, P)
-        P_prev.assign(P)
-        return t, P_prev, P
-
-
-def main():
-    V, u, v, dt, P_prev, theta_a, D_e, P_d, my_v, my_d, alpha_th, M_mm, q_h, T_s = simple_setup(simple_mesh)
-    a, L = construct_variation_problem(simple_mesh, u, v, dt, P_prev, theta_a, D_e, P_d, my_v, my_d, alpha_th, M_mm, q_h, T_s)
-    P = Function(V)
+def test_solve() -> None:
+    dt = 0.01
+    mesh = UnitIntervalMesh(10)
+    params = MassAirPhaseParameters(V=None, mesh=mesh, dt=dt)
+    mass_air_phase = MassAirPhase(params=params)
+    a, L = mass_air_phase.construct_variation_problem()
+    print(type(a))
+    print(type(L))
+    P = Function(params.V)
+    P_prev = params.P_prev
     num_steps = 10
     t = 0
     sols = []
     for n in range(num_steps):
-        t, P_prev, P = step(t, dt, P_prev, P, a, L)
-        sols.append(P.vector().array())
+        t, P_prev, P = step(time=t, dt=dt, p_prev=P_prev, p=P, a=a, l=L)
+        sols.append(P.vector().get_local())
     for s in sols:
-        print(s)
         plt.plot(s)
 
     plt.show()
+
+
 if __name__ == "__main__":
-    main()
+    test_solve()
