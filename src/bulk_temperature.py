4122d2bbf33a3439113c306d769a6aeeaccd462d
Ola Skavhaug <ola@xal.no>
2018-02-02 11:22:25 +0100

    Bulk temp py3 compat-ish

--- src/bulk_temperature.py
+++ src/bulk_temperature.py
@@ -1,42 +1,62 @@
-from fenics import *
-from snow_mesh import simple_mesh
+from typing import Optional, Tuple, Any, List
+from fenics import FunctionSpace
+from fenics import TrialFunction
+from fenics import TestFunction
+from fenics import Function
+from fenics import Constant
+from fenics import DirichletBC
+from fenics import Mesh
+from fenics import dot, grad, dx, rhs, lhs, near, solve
+from .snow_mesh import simple_mesh
 import matplotlib.pylab as plt
 
 
-def simple_setup(mesh, dt_val, V=None):
-    V = V or FunctionSpace(mesh, "Lagrange", 1)
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    T_prev = Function(V)
-    P_s = Constant(1.0)
-    c_s = Constant(1.0)
-    k_e = Constant(1.0)
-    Q_pc = Constant(1.0)
-    Q_sw = Constant(1.0)
-    Q_mm = Constant(1.0)
-    dt = Constant(dt_val)
-    return V, u, v, T_prev, P_s, c_s, k_e, Q_pc, Q_sw, Q_mm, dt
+class BulkTemperatureParameters:
+
+    def __init__(self, *, V: Optional[FunctionSpace], mesh: Mesh, dt: float) -> None:
+        V = V or FunctionSpace(mesh, "Lagrange", 1)
+        self.V = V
+        self.T = TrialFunction(V)
+        self.v = TestFunction(V)
+        self.T_prev = Function(V)
+        self.P_s = Constant(1.0)
+        self.c_s = Constant(1.0)
+        self.k_e = Constant(1.0)
+        self.Q_pc = Constant(1.0)
+        self.Q_sw = Constant(1.0)
+        self.Q_mm = Constant(1.0)
+        self.dt = Constant(dt)
 
 
-def construct_variation_problem(u, v, T_prev, P_s, c_s, k_e, Q_pc, Q_sw, Q_mm, dt):
-    u_t = (u - T_prev)/dt
-    f = Q_pc + Q_sw + Q_mm
-    F = P_s*c_s*u_t*v*dx + k_e*dot(grad(u), grad(v))*dx - f*v*dx
-    return lhs(F), rhs(F)
+
+class BulkTemperature:
+
+    def __init__(self, *, params: BulkTemperatureParameters) -> None:
+        self.params = params
+
+    def construct_variation_problem(self) -> Tuple[Any, Any]:
+        p = self.params
+        u_t = (p.T - p.T_prev)/p.dt
+        f = p.Q_pc + p.Q_sw + p.Q_mm
+        F = p.P_s*p.c_s*u_t*p.v*dx + p.k_e*dot(grad(p.T), grad(p.v))*dx - f*p.v*dx
+        return lhs(F), rhs(F)
 
 
-def surface_boundary(x, on_boundary):
-        return on_boundary and x[0] > 0
+    @staticmethod
+    def surface_boundary(x, on_boundary: bool) -> bool:
+            return on_boundary and x[0] > 0
 
 
-def ground_boundary(x, on_boundary):
-        return on_boundary and near(x[0], 0.0)
+    @staticmethod
+    def ground_boundary(x, on_boundary: bool) -> bool:
+            return on_boundary and near(x[0], 0.0)
 
 
-def make_bcs(V, surface_val, ground_val):
-    bc1 = DirichletBC(V, Constant(ground_val), ground_boundary)
-    bc2 = DirichletBC(V, Constant(surface_val), surface_boundary)
-    return [bc1, bc2]
+    @classmethod
+    def make_bcs(cls, V, surface_val, ground_val) -> List[DirichletBC]:
+        bc1 = DirichletBC(V, Constant(ground_val), cls.ground_boundary)
+        bc2 = DirichletBC(V, Constant(surface_val), cls.surface_boundary)
+        return [bc1, bc2]
 
 
 def step(t, dt, T_prev, T, a, L, bc):
@@ -47,14 +67,19 @@ def step(t, dt, T_prev, T, a, L, bc):
 
 
 def main():
-    V, u, v, T_prev, P_s, c_s, k_e, Q_pc, Q_sw, Q_mm, dt = simple_setup(simple_mesh)
-    a, L = construct_variation_problem(u, v, T_prev, P_s, c_s, k_e, Q_pc, Q_sw, Q_mm, dt)
+    p = BulkTemperatureParameters(V=None, mesh=simple_mesh, dt=0.01)
+    bulk_temp = BulkTemperature(params=p)
+    V = p.V
+    dt = p.dt
+    T_prev = p.T_prev
+    a, L = bulk_temp.construct_variation_problem()
+
     T = Function(V)
     num_steps = 10
     t = 0
     sols = []
     for n in range(num_steps):
-        bc = make_bcs(V, 0, -10)
+        bc = bulk_temp.make_bcs(V, 0, -10)
         t, T_prev, T = step(t, dt, T_prev, T, a, L, bc)
         sols.append(T.vector().array())
     for s in sols:
